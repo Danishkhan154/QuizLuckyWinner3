6d1581c9d6a3e2c66f5022f7f02610f8680a12cf
Marshall <shukla.prashant689@gmail.com>
2019-10-02 01:12:38 +0530

    Add files via upload

---
+++ luckywin/src/luckywin/user.java
@@ -0,0 +1,23 @@
+package luckywin;
+import java.util.*;
+public class user 
+{
+	String name;char ans;
+	Scanner sc=new Scanner(System.in);
+	
+	public void input()
+	{
+	System.out.println("Enter name\n");
+	name=sc.nextLine();
+	System.out.println("Command used to view os name\n");
+	System.out.println("a : os\n");
+	System.out.println("b : uname\n");
+	System.out.println("c : who\n");
+	System.out.println("d : osname\n");
+	ans= sc.next().charAt(0);
+	
+	
+	}
+	
+	
+}
